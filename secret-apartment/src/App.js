49fc5ec8a89cba9cba8dd4c9d7bec2ad77579efe
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2022-09-21 11:02:03 +0300

    pages

--- secret-apartment/src/App.js
+++ secret-apartment/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalContext } from "./context/global-context";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <GlobalContext.Provider value={{}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
+  );
 }
 
 export default App;
