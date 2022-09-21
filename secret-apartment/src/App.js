5b596cc5c856205b886ee76add6cec089e007335
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2022-09-21 11:09:30 +0300

    pages

--- secret-apartment/src/App.js
+++ secret-apartment/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalContext } from "./context/global-context";
+import Landing from "./pages/landing";
+import Main from "./pages/main";
+import Notification from "./pages/notification";
 
 function App() {
   return (
     <GlobalContext.Provider value={{}}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/" element={<Home />}/>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Landing />}/>
+          <Route path="/main" element={<Main />}/>
+          <Route path="/notification" element={<Notification />}/>
         </Routes>
       </BrowserRouter>
     </GlobalContext.Provider>
--- secret-apartment/src/pages/landing.jsx
+++ secret-apartment/src/pages/landing.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function landing() {
+function Landing() {
   return (
     <div>landing</div>
   )
 }
 
-export default landing;
+export default Landing;
