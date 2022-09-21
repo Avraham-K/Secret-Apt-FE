b55118f83ba4444570051f57b568ef86890ddd0d
Your Name <you@example.com>
2022-09-21 11:21:24 +0300

    Landing Page

---
+++ secret-apartment/src/pages/landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Geosuggest from 'react-geosuggest';
+
+export default function Landing(){
+
+
+    return(
+        <>
+        <div><h1>WELCOME TO SECRET APARTMENTS</h1></div>
+        <Geosuggest />
+        </>
+    )
+}
